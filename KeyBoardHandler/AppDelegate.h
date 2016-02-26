5be8a295c72730e45a8b35b5bf46666d01bab0ee
Sakkeer Hussain <sakkeer@qburst.com>
2016-02-26 10:59:34 +0530

    Initial Commit

---
+++ KeyBoardHandler/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KeyBoardHandler
+//
+//  Created by Sakkeer on 26/02/16.
+//  Copyright © 2016 Sample. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KeyBoardHandlerTests/KeyBoardHandlerTests.m
@@ -0,0 +1,39 @@
+//
+//  KeyBoardHandlerTests.m
+//  KeyBoardHandlerTests
+//
+//  Created by Sakkeer on 26/02/16.
+//  Copyright © 2016 Sample. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KeyBoardHandlerTests : XCTestCase
+
+@end
+
+@implementation KeyBoardHandlerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
