4d0dbfafcadc913558eb9e427a85893a6199d4b4
Sakkeer Hussain <sakkeer@qburst.com>
2016-02-26 14:37:54 +0530

    moved all contents to example folder

---
+++ example/KeyBoardHandler/ViewControllers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KeyBoardHandler
+//
+//  Created by Sakkeer on 26/02/16.
+//  Copyright © 2016 Sample. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "QBBaseViewController.h"
+
+@interface ViewController : QBBaseViewController
+
+@end
+
---
+++ example/KeyBoardHandler/ViewControllers/ViewController2.h
@@ -0,0 +1,14 @@
+//
+//  ViewController2.h
+//  KeyBoardHandler
+//
+//  Created by Sakkeer on 26/02/16.
+//  Copyright © 2016 Sample. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "QBBaseViewController.h"
+
+@interface ViewController2 : QBBaseViewController
+
+@end
